9fecaa80beda3e80ed8fe5a0d66ee28140cadc5e
Dhanraj9623 <dhanrajkadam5503@gmail.com>
2024-02-18 21:39:42 +0530

    working

--- emsBackend/src/main/java/com/project/ems/EmsApplication.java
+++ emsBackend/src/main/java/com/project/ems/EmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmsApplication.class, args);
+    }
 
 }
